df1c92fcdcaf22ed13263fc3fd1e9eb6a87bbe07
sonicyang <sonic.tw.tp@gmail.com>
2015-10-21 16:52:18 +0800

    Add API archive messages

--- cgi.py
+++ cgi.py
@@ -1,9 +1,18 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-        return "Hello World!"
+    return "Hello World!"
+
+@app.route("/append")
+def append():
+    open("data", "a").write(str(request.args.get("msg")) + "\n\r")
+    return ""
+
+@app.route("/retreive")
+def retreive():
+    return open("data").read()
 
 if __name__ == "__main__":
     app.run( host= '0.0.0.0' )
