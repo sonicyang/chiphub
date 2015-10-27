8e95417299149f540440d8e6a251344f410bdffa
Sonic Yang <sonic.tw.tp@gmail.com>
2015-10-28 00:40:56 +0800

    Add multiple token fields on login user model for security purpose

--- login/models.py
+++ login/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 
 class Users(models.Model):
+    token = models.CharField(max_length=40)
     username = models.CharField(max_length=20)
     email = models.CharField(max_length=50)
     login_service = models.CharField(max_length=20)
-    token = models.CharField(max_length=50)
+    access_token = models.CharField(max_length=50)
+    refresh_token = models.CharField(max_length=50)
     default_shipping_address = models.CharField(max_length=100)
     phone_number = models.CharField(max_length=20)
     roc_id = models.CharField(max_length=10)
