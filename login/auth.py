366826238ca4baff8aa299eab02cad6b1b5b9b6d
sonicyang <sonic.tw.tp@gmail.com>
2015-10-29 12:32:23 +0800

    Use token to represent the login status

--- login/auth.py
+++ login/auth.py
@@ -5,15 +5,23 @@ import requests
 import json
 import random
 from keys import keys
+from uuid import UUID
 
 login_providers = []
+
+def validate_uuid4(uuid_string):
+    try:
+        val = UUID(uuid_string, version=4)
+    except ValueError:
+        return False
+
+    return True
 
 def generate_token(email):
     url = "https://api.random.org/json-rpc/1/invoke"
     headers = {'content-type': 'application/json'}
     ID = random.randint(0, 65536)
 
-    #XXX: hide API_key
     payload = {
             "jsonrpc": "2.0",
             "method": "generateUUIDs",
@@ -29,13 +37,21 @@ def generate_token(email):
 
     assert response['id'] == ID
     assert 'result' in response
+    assert validate_uuid4(response['result']['random']['data'])
 
     return response['result']['random']['data']
 
 def authuicate(request, user_email):
-    user = Users.objects.get(email = user_email)
-    request.session['logined'] = True
-    request.session['token'] = user.token
+    try:
+        user = Users.objects.get(email = user_email)
+        request.session['token'] = user.token
+        return True
+    except ObjectDoesNotExist:
+        if 'token' in request.session:
+            del resuest.sesion['token']
+            request.session.modified = True
+        return False
+
 
 def register(user_data):
     user_data.token = generate_token(user_data.email)
@@ -49,16 +65,21 @@ def hasUser(user_email):
         return False
 
 def isLogin(request):
-    if 'logined' not in request.session:
+    if 'token' not in request.session:
         return False
     else:
-        return request.session['logined']
+        if validate_uuid4(request.sesion['token']):
+            return True
+        else:
+            del request.sesion['token']
+            request.session.modified = True
+            return False
 
 def get_session_token(request):
     if isLogin(request):
         return request.session['token']
     else:
-        raise ''
+        return ''
 
 def get_user_data(user_token):
     return Users.objects.filter(token = user_token)
