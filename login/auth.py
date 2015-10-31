9b8c8689d115f8a87400c3c4b7e3598b10a50758
Sonic Yang <sonic.tw.tp@gmail.com>
2015-10-31 21:02:03 +0800

    REfine the way to register new users, matching using uuid

--- login/auth.py
+++ login/auth.py
@@ -60,13 +60,15 @@ def create_session(request, uuid):
             request.session.modified = True
         return False
 
-def create_empty_user(uuid, service_provider, access_token, **additional):
+def create_empty_user(unique, service_provider, access_token, **additional):
+    uuid = generate_static_uuid(unique)
+
     try:
         user = Users.objects.get(uuid = uuid)
         if user.username is None:
             user.delete()
         else:
-            return False
+            return None
     except ObjectDoesNotExist:
         pass
 
@@ -79,12 +81,12 @@ def create_empty_user(uuid, service_provider, access_token, **additional):
 
     user.save()
 
-    return True
+    return uuid
 
 def register_data(user_data):
-    empty_user = Users.objects.get(email = user_data.email)
+    empty_user = Users.objects.get(uuid = user_data.uuid)
 
-    assert empty_user.token == None
+    assert empty_user.username == None
 
     empty_user.token = generate_random_uuid()
     empty_user.email = user_data.email
