18ff049123f843708d5f9b2d77c0520e8a860a4a
anarchih <anarchih.tw@gmail.com>
2015-11-07 23:22:02 +0800

    change hide-show effect to fadein-fadeout effect of login-wrapper

--- static/js/base.js
+++ static/js/base.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){
 	$("#login_button").click(function(){
-        $('#login_wrapper').show(700)
+        $('#login_wrapper').fadeIn(700)
 
         return false; //To prevent browser appending a # on to the URL
 	});
 
 	$("#login_wrapper").click(function(){
-        $('#login_wrapper').hide(700)
+        $('#login_wrapper').fadeOut(700)
 	});
 });
 
