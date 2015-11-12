744b4e5fef1e6a516f0024715ced0e06ebacd2c0
Alan Audi <alanaudi@tiger.c4labs.xyz>
2015-11-13 00:27:40 +0800

    homepage

--- static/js/base.js
+++ static/js/base.js
@@ -1,29 +1,63 @@
+// Login Form 1
+/*
 $(document).ready(function(){
-	$("#login_button").click(function(){
-        $('#login_wrapper').fadeIn(700)
+  $("#login_button").click(function(){
+    $('#login_wrapper').fadeIn(700)
 
-        return false; //To prevent browser appending a # on to the URL
-	});
+      return false; //To prevent browser appending a # on to the URL
+  });
 
-	$("#login_wrapper").click(function(){
-        $('#login_wrapper').fadeOut(700)
-	});
+  $("#login_wrapper").click(function(){
+    $('#login_wrapper').fadeOut(700)
+  });
 
-    $.ajax({
-        url: "/islogin",
-        statusCode: {
-            200: function(){
-                $("#login_button_wrapper").hide()
-                $("#logout_button_wrapper").show()
-                $("#profile_button_wrapper").show()
-            },
-            400: function(){
-                $("#login_button_wrapper").show()
-                $("#logout_button_wrapper").hide()
-                $("#profile_button_wrapper").hide()
+  $.ajax({
+    url: "/islogin",
+    statusCode: {
+      200: function(){
+        $("#login_button_wrapper").hide()
+          $("#logout_button_wrapper").show()
+          $("#profile_button_wrapper").show()
+      },
+      400: function(){
+        $("#login_button_wrapper").show()
+          $("#logout_button_wrapper").hide()
+          $("#profile_button_wrapper").hide()
 
-            }
-        }
-    })
+      }
+    }
+  })
 });
+*/
+
+// Login Form 2
+$(document).ready(function(){
+  $("#login_button").click(function(){
+    $('#login_wrapper').fadeIn(700)
+
+      return false; //To prevent browser appending a # on to the URL
+  });
+
+  $("#login_wrapper").click(function(){
+    $('#login_wrapper').fadeOut(700)
+  });
+
+  $.ajax({
+    url: "/islogin",
+    statusCode: {
+      200: function(){
+        $("#login_button_wrapper").hide()
+          $("#logout_button_wrapper").show()
+          $("#profile_button_wrapper").show()
+      },
+      400: function(){
+        $("#login_button_wrapper").show()
+          $("#logout_button_wrapper").hide()
+          $("#profile_button_wrapper").hide()
+
+      }
+    }
+  })
+});
+
 
