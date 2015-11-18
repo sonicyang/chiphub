7d19743ca711ce62e99274315bce6045b7c8d454
alanaudi <alan.audi.micro@hotmail.com>
2015-11-18 23:11:22 +0800

    add progress page

---
+++ static/js/progress.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+  function modifValues(){
+    var val = $('.downloading-progress-bar').attr('data-value');
+    if(val>=100){val=5;}
+    var newVal = val*1+0.5;
+    var txt = Math.floor(newVal)+'%';
+
+    $('.downloading-progress-bar').attr('data-value',newVal);
+    $('.percentage').html(txt);
+    $('.downloading-progress-bar').css("width", txt);
+  }
+
+  setInterval(function(){ modifValues(); },50);
+
+});
