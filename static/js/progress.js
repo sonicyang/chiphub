6ae2acbb0413c49620cf193e4384d96f5c1b2402
alanaudi <alan.audi.micro@hotmail.com>
2015-11-20 16:44:14 +0800

    progress bar version2

--- static/js/progress.js
+++ static/js/progress.js
@@ -1,17 +1,38 @@
-$(document).ready(function() {
+CountDownTimer('12/20/2014 10:1 AM', 'countdown');
+CountDownTimer('12/20/2014 10:1 AM', 'newcountdown');
 
-  function modifValues(){
+function CountDownTimer(dt, id)
+{
+	var end = new Date(dt);
 
-    var val = $('.downloading-progress-bar').attr('data-value');
-    if(val>=100){val=5;}
-    var newVal = val*1+0.5;
-    var txt = Math.floor(newVal)+'%';
+	var _second = 1000;
+	var _minute = _second * 60;
+	var _hour = _minute * 60;
+	var _day = _hour * 24;
+	var timer;
 
-    $('.downloading-progress-bar').attr('data-value',newVal);
-    $('.percentage').html(txt);
-    $('.downloading-progress-bar').css  ("width", txt);
-  }
+	function showRemaining() {
+		var now = new Date();
+		var distance = end - now;
+		if (distance < 0) {
 
-  setInterval(function(){ modifValues(); },50);
+			clearInterval(timer);
+			document.getElementById(id).innerHTML = '0';
 
-});
+			return;
+		}
+		var days = Math.floor(distance / _day);
+		var hours = Math.floor((distance % _day) / _hour);
+		var minutes = Math.floor((distance % _hour) / _minute);
+		var seconds = Math.floor((distance % _minute) / _second);
+
+		document.getElementById(id).innerHTML = days /*+ ' days'*/;
+		//document.getElementById(id).innerHTML += hours + 'hrs ';
+		//document.getElementById(id).innerHTML += minutes + 'mins ';
+		//document.getElementById(id).innerHTML += seconds + 'secs';
+	}
+
+	timer = setInterval(showRemaining, 1000);
+}
+
+
