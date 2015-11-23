00abab8b00a9b3c769bf772db10a7a0cccc36b65
Sonic Yang <sonic.tw.tp@gmail.com>
2015-11-23 15:18:58 +0800

    Remove redundant Counter code in progress.js

--- static/js/progress.js
+++ static/js/progress.js
@@ -1,5 +1,3 @@
-CountDownTimer('12/20/2014 10:1 AM', 'countdown');
-CountDownTimer('12/20/2014 10:1 AM', 'newcountdown');
 var GOAL = 3000;
 var current_rally = 0;
 var rally_person_count = 0;
@@ -12,39 +10,6 @@ $(document).ready(function(){
         update_price();
     })
 })
-
-function CountDownTimer(dt, id)
-{
-	var end = new Date(dt);
-
-	var _second = 1000;
-	var _minute = _second * 60;
-	var _hour = _minute * 60;
-	var _day = _hour * 24;
-	var timer;
-
-	function showRemaining() {
-		var now = new Date();
-		var distance = end - now;
-		if (distance < 0) {
-
-			clearInterval(timer);
-
-			return;
-		}
-		var days = Math.floor(distance / _day);
-		var hours = Math.floor((distance % _day) / _hour);
-		var minutes = Math.floor((distance % _hour) / _minute);
-		var seconds = Math.floor((distance % _minute) / _second);
-
-		document.getElementById(id).innerHTML = days /*+ ' days'*/;
-		//document.getElementById(id).innerHTML += hours + 'hrs ';
-		//document.getElementById(id).innerHTML += minutes + 'mins ';
-		//document.getElementById(id).innerHTML += seconds + 'secs';
-	}
-
-	timer = setInterval(showRemaining, 1000);
-}
 
 function update_price(){
     document.getElementById("funds-count").innerHTML = rally_person_count;
