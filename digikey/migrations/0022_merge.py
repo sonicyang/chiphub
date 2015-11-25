6917e3b8689ace77bda912f82163f0f91a1a881b
Sonic Yang <sonic.tw.tp@gmail.com>
2015-11-26 01:40:00 +0800

    Solve conflict in DB schema for circle CI

---
+++ digikey/migrations/0022_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('digikey', '0019_merge'),
+        ('digikey', '0021_auto_20151125_1353'),
+    ]
+
+    operations = [
+    ]
