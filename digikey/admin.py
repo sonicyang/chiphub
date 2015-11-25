8065bfa634c1d463096674f53ee5e72e0e049994
Sonic Yang <sonic.tw.tp@gmail.com>
2015-11-25 22:16:09 +0800

    Flourish data displied in order group admin console

--- digikey/admin.py
+++ digikey/admin.py
@@ -30,11 +30,23 @@ class OrderAdmin(admin.ModelAdmin):
 @admin.register(Groups)
 class GroupAdmin(admin.ModelAdmin):
     list_filter = ['ordered', 'orderdate']
+    list_display = ('uuid', 'ordered','orderdate', 'get_accumulated')
     fieldsets = [
-                (None,               {'fields': ['ordered', 'orderdate']}),
+                (None,               {'fields': ['uuid', 'ordered', 'orderdate']}),
     ]
 
     inlines = [OrderInline]
+
+    def get_accumulated(self, obj):
+        total = 0
+        for order in Orders.objects.all().filter(group_id = obj, paid = True):
+            for component in order.components_set.all():
+                detail = Order_Details.objects.get(order = order, component = component)
+
+                total += detail.quantity * component.unit_price
+
+        return total
+    get_accumulated.short_description = 'Accumulated'
 
 @admin.register(Components)
 class ComponentAdmin(admin.ModelAdmin):
