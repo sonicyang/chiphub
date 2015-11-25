b7bbe9443de3b021a199ff0775396bf0b8387be9
Sonic Yang <sonic.tw.tp@gmail.com>
2015-11-25 20:15:53 +0800

    merge conflict DB schema

---
+++ digikey/migrations/0020_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('digikey', '0019_auto_20151125_0949'),
+        ('digikey', '0014_auto_20151125_0857'),
+    ]
+
+    operations = [
+    ]
