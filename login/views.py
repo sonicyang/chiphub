1289c56d0af644b90c15ca0f07312f51cac11b77
Sonic Yang <sonic.tw.tp@gmail.com>
2015-11-26 01:16:20 +0800

    redirect to progress page instead of main after profile

--- login/views.py
+++ login/views.py
@@ -24,7 +24,7 @@ def logout(request):
     if isLogin(request):
         auth.close_session(request, auth.get_session_token(request))
 
-    return redirect('main.views.index')
+    return redirect('digikey.views.progress')
 
 def update_profile(request):
     if auth.isLogin(request):
@@ -40,10 +40,10 @@ def update_profile(request):
 
         if not auth.hasProfile(uuid):
             auth.register_data(uuid, user_profile)
-            return redirect('main.views.index')
         else:
             auth.update_data(uuid, user_profile)
-            return redirect('login.views.profile')
+
+        return redirect('digikey.views.progress')
 
 def google_login(request):
     #XXX: large overhaed to create objects
