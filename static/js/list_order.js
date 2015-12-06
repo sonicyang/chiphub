b8d184cf0abb15f444e401a3b677925dbbfde99c
anarchih <anarchih.tw@gmail.com>
2015-12-06 15:22:58 +0800

    implement 'look details of the order' feature.

--- static/js/list_order.js
+++ static/js/list_order.js
@@ -2,6 +2,7 @@ var data;
 var order_html = "\
     <div class=\"order\">\
         <div class=\"container-fluid\">\
+            <a class=\"order-info-link\" href=\"#\">link</a>\
             <div class=\"row\">\
                 <div class=\"col-md-5\">\
                     <h2 class=\"order-date\" type=\"date\">2015/12/31</h2>\
@@ -43,6 +44,39 @@ var order_html = "\
     <hr />\
 ";
 
+var chip_html = "\
+    <div class=\"row chip invoice-module\">\
+        <div class=\"\">\
+            <div class=\"col-md-6\">\
+                <h3>CHIP</h3>\
+                <div class=\"invoice-for part-number\">\
+                </div>\
+            </div>\
+        </div>\
+        <div class=\"\">\
+            <div class=\"col-md-6\">\
+                <h3>單價</h3>\
+                <div class=\"invoice-for unit-price\">\
+                </div>\
+            </div>\
+        </div>\
+        <div class=\"\">\
+            <div class=\"col-md-6\">\
+                <h3>數量</h3>\
+                <div class=\"invoice-for quantity\">\
+                </div>\
+            </div>\
+        </div>\
+        <div class=\"\">\
+            <div class=\"col-md-6\">\
+                <h3>總價 (單價X數量)</h3>\
+                <div class=\"invoice-for chip-total-price\">\
+                </div>\
+            </div>\
+        </div>\
+    </div>\
+"
+var data_list = []
 $.get("/user_history_digikey/", function(d){
     list = JSON.parse(d);
     list = list.reverse();
@@ -51,11 +85,13 @@ $.get("/user_history_digikey/", function(d){
         for (var i = 0; i < list.length; i ++){
             order_list.append(order_html);
         }
+        counter = 0
         $('.order').each(function(index){
             var item = $(this)
             $.get("/order_info_digikey?UUID=" + list[index], function(d){
                 data = JSON.parse(d)
-
+                data_list[index] = data
+                counter += 1
                 item.find('.order-date').each(function(){
                     var type = $(this).attr("type");
                     var text = data[type];
@@ -77,7 +113,6 @@ $.get("/user_history_digikey/", function(d){
                     }
                     $(this).text(text);
                 })
-
                 item.find(".paid").each(function(){
                     var type = $(this).attr("type");
                     var text = data[type];
@@ -86,6 +121,11 @@ $.get("/user_history_digikey/", function(d){
                     }
                     $(this).text(text);
                 })
+                item.find(".order-info-link").attr("index", index)
+
+                if(counter == list.length){
+                    addEventForLinks()
+                }
             })
         })
         //$("label[type=shipping_address]").each(function(index){
@@ -117,3 +157,28 @@ $.get("/user_history_digikey/", function(d){
     })
 
 })
+function addEventForLinks(){
+    $(".order-info-link").click(function(e){
+        $(".chip").remove()
+        var ind = $(this).attr("index")
+        $("#info-shipping-address").text(data_list[ind]["shipping_address"])
+        $("#info-sent-date").text(data_list[ind]["sent_date"])
+        $("#info-net").text(data_list[ind]["net"])
+        $("#info-paid-date").text(data_list[ind]["paid_date"])
+        $("#info-paid-account").text(data_list[ind]["paid_account"])
+
+        for (var i = 0; i < data_list[ind]['components'].length; i ++){
+            $("#chip-list").append(chip_html)
+        }
+        $("#chip-list .chip").each(function(){
+            var chip = $(this)
+            var comp = data_list[ind]['components'][chip.index()]
+            chip.find(".part-number").text(comp['part_number'])
+            chip.find(".unit-price").text(comp['unit_price'])
+            chip.find(".quantity").text(comp['quantity'])
+            chip.find(".chip-total-price").text(comp['quantity'] * comp['unit_price'])
+        })
+        $("#order-info").show()
+    })
+
+}
