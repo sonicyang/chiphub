676f82dc4a92b3dfd709b9cd0a1368a2b8224d26
anarchih <anarchih.tw@gmail.com>
2015-12-08 23:58:55 +0800

    implement front-end top100 feature

---
+++ static/js/chatroom.js
@@ -0,0 +1,90 @@
+var family_html = "\
+<div  class=\"family\">\
+    <h3 class=\"family-mname\"></h3>\
+    <h5 class=\"family-sname\"></h5>\
+</div>\
+<hr />\
+"
+
+var chip_card_html = "\
+        <label class=\"chip-card bg-info\">\
+            <a href=\"#\">\
+                QWERTYQ\
+            </a>\
+        </label>\
+"
+
+String.prototype.hashCode = function() {
+    var hash = 0, i, chr, len;
+    if (this.length === 0) return hash;
+    for (i = 0, len = this.length; i < len; i++) {
+        chr   = this.charCodeAt(i);
+        hash  = ((hash << 5) - hash) + chr;
+        hash |= 0; // Convert to 32bit integer
+    }
+    return hash;
+};
+
+$("#search").click(function(e){
+    search(e);
+})
+
+function search(e){
+    var pn = $("#part-number").text();
+}
+
+$.get("/chatroom/top100/")
+.success(function(data){
+    var pk_list = JSON.parse(data);
+    getPNs(pk_list);
+})
+.error(function(d){
+    alert("Error !");
+})
+
+function getPNs(pk_list){
+    var pn_info_list = [];
+    var counter = 0;
+    for (var i = 0; i < pk_list.length; i ++){
+        (function(x){
+            $.get("/chatroom/get_component_info/",
+                  {"pk": pk_list[x]})
+                  .success(function(data){
+                      pn_info_list[x] = JSON.parse(data);
+                      counter += 1
+                      if (counter == pk_list.length){
+                          showPNs(pn_info_list)
+                      }
+                  })
+        })(i);
+    }
+}
+
+function showPNs(pn_info_list){
+    var pn_info_dict = {};
+    for (var i = 0; i < pn_info_list.length; i ++){
+        var family = pn_info_list[i]['ctype']['mname'] + pn_info_list[i]['ctype']['sname'];
+        var name = pn_info_list[i]['common_name'];
+        if (pn_info_dict[name] !== undefined){
+            appendChipCard(pn_info_list[i]);
+            pn_info_dict[name] = 1;
+        }else{
+            appendFamily(pn_info_list[i]);
+            pn_info_dict[name] += 1;
+        }
+    }
+}
+function appendFamily(pn){
+    var family_name = pn['ctype']['mname'] + pn['ctype']['sname'];
+    $("#chip-card-list").append(family_html);
+    $("#chip-card-list .family").last().attr("name", family_name.hashCode())
+    $("#chip-card-list .family").last().find(".family-mname").text(pn['ctype']['mname'])
+    $("#chip-card-list .family").last().find(".family-sname").text(pn['ctype']['sname'])
+    appendChipCard(pn);
+}
+function appendChipCard(pn){
+    var family_name = pn['ctype']['mname'] +  pn['ctype']['sname'];
+    var family_pattern = ".family" + "[name=" + family_name.hashCode() + "]"
+    $(family_pattern).append(chip_card_html);
+    $(family_pattern+ " label a").last().text(pn['common_name']);
+}
