0f8b8a3996f72585bc865874a9f41c795f5c474d
sonicyang <sonic.tw.tp@gmail.com>
2015-12-08 15:22:17 +0800

    add basic API function skleton

--- chatroom/views.py
+++ chatroom/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+def chatroom(request):
+    return render(request, 'chatroom.html')
+
 def append(request):
     # open("data", "a").write(str(request.args.get("msg")) + "\n\r")
     open("/tmp/data", "ab").write(request.GET['msg'].encode('utf8') + "\n\r".encode('utf-8'))
@@ -11,6 +14,26 @@ def retreive(request):
     payload = fil.read()
     return HttpResponse(payload)
 
-def chatroom(request):
-    return render(request, 'chatroom.html')
+def search(request):
+    return HttpResponse("")
 
+def get_component_comments(request):
+    return HttpResponse("")
+
+def add_component_comment(request):
+    return HttpResponse("")
+
+def del_component_comment(request):
+    return HttpResponse("")
+
+def edit_component_comment(request):
+    return HttpResponse("")
+
+def rank_comment(request):
+    return HttpResponse("")
+
+def rank_entry(request):
+    return HttpResponse("")
+
+def add_entry(request):
+    return HttpResponse("")
