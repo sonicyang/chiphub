69527da1d95dd398b6e4fd41d044d0540bd02386
sonicyang <sonic.tw.tp@gmail.com>
2015-12-13 08:22:17 +0800

    Add back commenting feature

--- static/js/chatroom.js
+++ static/js/chatroom.js
@@ -2,11 +2,24 @@ app.controller('chatroom-ctrl', function($scope, $http, $rootScope, $document) {
     $scope.infoing = false;
     $scope.families = [];
     $scope.comments = [];
+    $scope.comment_text = "";
 
     $http.get("/chatroom/top100/")
         .then(function(response){
             $scope.families = response.data;
         });
+
+    $scope.submitComment= function(chip){
+        //XXX: Use Post
+        $http.get("/chatroom/add_component_comment?pk=" + chip.id + "&content=" + $scope.comment_text)
+            .then(function(response){
+                $scope.comment_text = "";
+                $http.get("/chatroom/get_component_comments?pk=" + chip.id)
+                    .then(function(response){
+                        $scope.comments = response.data;
+                    });
+            });
+    };
 
     $scope.show_info = function(family, chip){
         $scope.infoing = true;
