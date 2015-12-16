310a3d6aa662fb205a68b99b1fe508726c0a2ef5
sonicyang <sonic.tw.tp@gmail.com>
2015-12-16 17:09:13 +0800

    Link to remote PosgreSQL server

--- chiphub/settings.py
+++ chiphub/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+from keys.keys import DB_KEY
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -82,8 +83,12 @@ WSGI_APPLICATION = 'chiphub.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'chiphub',
+        'USER': 'chiphub',
+        'PASSWORD': DB_KEY,
+        'HOST': 'c4labs.xyz',
+        'PORT': '5432',
     }
 }
 
